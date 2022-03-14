574346abc2dea3aa95d870298a044524f51b171a
bossteeh <akinrinolaelijah@gmail.com>
2022-03-14 13:09:58 +0100

    learnt how to use alert and username prompt

--- main.js
+++ main.js
@@ -1,4 +1,50 @@
 var x = 5;
 var y = 6;
 var z = x + y;
-console.log(z)
+console.log(z+x)
+
+let c = 1;
+let b = 8;
+let u = c + b;
+console.log(u)
+
+// let carName = "Volvo";
+// document.getElementById("Demo").innerHTML = carName;
+
+
+
+var username = prompt("Enter your name:");
+document.getElementById("name").innerHTML = username;
+
+alert ("Welcome to ATC " + username)
+
+var question = prompt("Enter your age:");
+document.getElementById("age").innerHTML = question;
+
+var question = prompt("Enter your address:");
+document.getElementById("address").innerHTML = question;
+
+var question = prompt("Enter your phone-number:");
+document.getElementById("number").innerHTML = question;
+
+// var username = prompt("Enter your name:");
+// document.getElementById("name").innerHTML = username;
+
+// let myname = "Tolu"
+
+
+let num1 =9, num2=3;
+console.log("*********")
+console.log(num1 * num2)
+console.log(num1 - num2)
+console.log(num1 + num2)
+console.log(num1 / num2)
+console.log(num1 ** num2)
+console.log(num1 % num2)
+
+
+/* 
+ASSIGNMENT
+
+Write a program with javascript to calculate the number of seconds in a week 
+// then display your  answer on the html webpage */
